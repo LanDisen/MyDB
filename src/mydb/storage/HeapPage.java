17c967ad6ea011f0a23dd17e506005d8844b4588
Lann <3530306071@qq.com>
2023-08-05 11:33:33 +0800

    实现HeapPage与dirty和元组插入删除相关的操作

--- src/mydb/storage/HeapPage.java
+++ src/mydb/storage/HeapPage.java
@@ -33,14 +33,22 @@ public class HeapPage implements Page {
      * 槽的数量
      */
     final int slotsNum;
+    /**
+     * 当前空槽数量
+     */
+    int emptySlotsNum;
 
     byte[] oldData;
     private final Byte oldDataLock = (byte) 0;
+
+    boolean dirty;
+    TransactionId tid;
 
     public HeapPage(HeapPageId id, byte[] data) throws IOException {
         this.pid = id;
         this.tupleDesc = Database.getCatalog().getTupleDesc(id.getTableId());
         this.slotsNum = getTuplesNum();
+        this.emptySlotsNum = slotsNum;
         DataInputStream dis = new DataInputStream(new ByteArrayInputStream(data));
         // 对该页面的头部槽进行分配并读取
         this.header = new byte[getHeaderSize()];
@@ -57,15 +65,19 @@ public class HeapPage implements Page {
             e.printStackTrace();
         }
         dis.close();
-        //
+        // TODO image
+        this.dirty = false;
+        this.tid = null;
     }
 
     /**
      * @return 返回该页面的元组（tuple）数量
      */
     private int getTuplesNum() {
-        // TODO
-        return 0;
+        int pageSize = BufferPool.getPageSize(); // 页面字节大小
+        // tupleDesc.getSize()为每个元组所占字节数量
+        int tuplesNum = (int) Math.floor(pageSize * 8 * 1.0 / (tupleDesc.getSize() * 8 + 1));
+        return tuplesNum;
     }
 
     /**
@@ -73,14 +85,14 @@ public class HeapPage implements Page {
      * @return 返回计算得到的页面头部字节数
      */
     private int getHeaderSize() {
-        // TODO
-        return 0;
+        // header为bitmap，一个tuple占一位
+        int headerSize = (int) Math.ceil(getTuplesNum() * 1.0 / 8);
+        return headerSize;
     }
 
     @Override
     public HeapPageId getId() {
-        // TODO
-        return null;
+        return pid;
     }
 
     /**
@@ -152,23 +164,65 @@ public class HeapPage implements Page {
         return new byte[pageSize]; // 空页面用全0填充
     }
 
+    /**
+     * 从该页面中删除指定的元组。需要相应更新页面的header
+     * @param tuple 需要删除的元组
+     * @throws DbException 若该元组不在页面中，或元组对应的槽为空，会抛出数据库异常
+     */
     public void deleteTuple(Tuple tuple) throws DbException {
-        // TODO
+        RecordId recordId = tuple.getRecordId();
+        int slotIndex = recordId.getTupleNo();
+        if (recordId.getPageId() != this.pid || !isSlotUsed(slotIndex)) {
+            throw new DbException("tuple is not in this page");
+        }
+        // 成功删除元组，对应的槽设置为未使用
+        setSlotUsed(slotIndex, false);
+        tuples[slotIndex] = null;
+        emptySlotsNum++;
     }
 
+    /**
+     * 将指定的元组插入该页面，并相应更新页面header
+     * @param tuple 需要新增的元组
+     * @throws DbException 若页面已满（无空槽），或者TupleDesc不匹配会抛出异常
+     */
     public void insertTuple(Tuple tuple) throws DbException {
-        // TODO
+        TupleDesc tempTupleDesc = tuple.getTupleDesc();
+        if (getEmptySlotsNum() == 0) {
+            throw new DbException("this page is full");
+        }
+        if (!tempTupleDesc.equals(this.tupleDesc)) {
+            throw new DbException("the tupleDesc is not matched");
+        }
+        // 找一个空的槽插入新元组
+        for (int i=0; i<slotsNum; i++) {
+            if (!isSlotUsed(i)) {
+                setSlotUsed(i, true);
+                tuple.setRecordId(new RecordId(this.pid, i));
+                this.tuples[i] = tuple;
+                this.emptySlotsNum--;
+                return;
+            }
+        }
     }
 
+    /**
+     * @return 页面dirty会返回对应的事务ID，否则返回null
+     */
     @Override
     public TransactionId isDirty() {
-        // TODO
-        return null;
+        return dirty ? tid: null;
     }
 
+    /**
+     * 将该页面设为dirty或not dirty，并记录对应的事务ID
+     * @param dirty true为设置dirty，false为设置not dirty
+     * @param tid 事务ID
+     */
     @Override
     public void setDirty(boolean dirty, TransactionId tid) {
-        // TODO
+        this.dirty = dirty;
+        this.tid = tid;
     }
 
     /**
@@ -207,30 +261,49 @@ public class HeapPage implements Page {
      * @return 返回页面中空槽的数量
      */
     public int getEmptySlotsNum() {
-        // TODO
-        return 0;
+        return this.emptySlotsNum;
     }
 
     /**
-     * @return 若槽已被使用则返回true，否则返回false
+     * @param index 槽的索引
+     * @return 若对应索引的槽已被使用则返回true，否则返回false
      */
     public boolean isSlotUsed(int index) {
-        // TODO
-        return false;
+        // 找到槽在bitmap中对应的位置
+        int byteIndex = index / 8;
+        int bitIndex = index % 8;
+        int isUsed = (header[byteIndex] >> bitIndex) & 1;
+        return (isUsed == 1);
     }
 
     /**
-     * 用于标记槽被使用
+     * 用于标记槽是否被使用，同时会更新header
+     * @param index 槽的索引
+     * @param value true为将该槽设置为被使用，false为设置为空槽
      */
     private void setSlotUsed(int index, boolean value) {
-        // TODO
+        byte b = header[Math.floorDiv(index, 8)]; // 得到bitmap所在的一行字节
+        byte mask = (byte) (1 << (index % 8));
+        if (value) {
+            // 将该槽设置为被使用
+            header[Math.floorDiv(index, 8)] = (byte) (b | mask);
+        } else {
+            // 设置为空槽
+            header[Math.floorDiv(index, 8)] = (byte) (b & (~mask));
+        }
     }
 
     /**
      * @return 返回该页面所有元组的迭代器（不能返回空槽中的元组）
      */
     public Iterator<Tuple> iterator() {
-        // TODO
-        return null;
+        List<Tuple> tupleList = new ArrayList<>();
+        // 将非空的槽对应元组加入到迭代器中
+        for (int i=0; i<slotsNum; i++) {
+            if (isSlotUsed(i)) {
+                tupleList.add(this.tuples[i]);
+            }
+        }
+        return tupleList.iterator();
     }
 }
