8bdea37cc5b17c28c4667184b79e23b44f99a609
Lann <3530306071@qq.com>
2023-08-06 23:36:04 +0800

    Catalog实现loadSchema方法

--- src/mydb/common/Catalog.java
+++ src/mydb/common/Catalog.java
@@ -1,28 +1,18 @@
 package mydb.common;
 
 import mydb.storage.DbFile;
+import mydb.storage.HeapFile;
 import mydb.storage.TupleDesc;
 import mydb.storage.Table;
 
 import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
+import java.io.*;
 
 /**
  * Catalog类用来跟踪数据库中的表（tables）和它们对应的模式（schemas）
  */
 public class Catalog {
-
-//    public class Table {
-//        private DbFile dbFile;
-//        private String name;
-//        private String primaryKeyName;
-//
-//        public Table(DbFile dbFile, String name, String primaryKeyName) {
-//            this.dbFile = dbFile;
-//            this.name = name;
-//            this.primaryKeyName = primaryKeyName;
-//        }
-//    }
 
     /**
      * 通过DbFile的getId()作为key获得对应的Table
@@ -123,20 +113,60 @@ public class Catalog {
         tableName2Id.clear();
     }
 
-//    /**
-//     * 从catalogFile中读取schema并在数据库中创建对应的表
-//     */
-//    public void loadSchema(String catalogFile) {
-//        String line = "";
-//        String baseFolder=new File(new File(catalogFile).getAbsolutePath()).getParent();
-//        try {
-//            BufferedReader br = new BufferedReader(new FileReader(catalogFile));
-//
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//            System.exit(0);
-//        }
-//    }
-
-
+    /**
+     * 从catalogFile中读取schema并在数据库中创建对应的表
+     */
+    public void loadSchema(String catalogFile) {
+        String line = "";
+        // 放置数据库文件的目录
+        String baseFolder = new File(new File(catalogFile).getAbsolutePath()).getParent();
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(catalogFile));
+            while ((line = br.readLine()) != null) {
+                // line字符串的格式：tableName(field type, field type, ...)
+                String tableName = line.substring(0, line.indexOf("(")).trim();
+                String res = line.substring(line.indexOf("(") + 1, line.indexOf(")")).trim();
+                String[] fields = res.split(",");
+                ArrayList<String> names = new ArrayList<>();
+                ArrayList<Type> types = new ArrayList<>();
+                String primaryKeyName = "";
+                for (String field: fields) {
+                    String[] nameType = field.trim().split(" ");
+                    names.add(nameType[0].trim());
+                    String type = nameType[1].trim();
+                    if (type.equalsIgnoreCase("int")) {
+                        types.add(Type.INT_TYPE);
+                    } else if (type.equalsIgnoreCase("string")) {
+                        types.add(Type.STRING_TYPE);
+                    } else {
+                        System.out.println("Unknown field type " + type);
+                        System.exit(0);
+                    }
+                    if (nameType.length == 3) {
+                        // primary key
+                        if (nameType[2].trim().equals("pk")) {
+                            // 该字段为主键
+                            primaryKeyName = nameType[0].trim();
+                        } else {
+                            System.out.println("Unknown annotation " + nameType[2]);
+                            System.exit(0);
+                        }
+                    }
+                }
+                Type[] typeArr = types.toArray(new Type[0]);
+                String[] nameArr = names.toArray(new String[0]);
+                TupleDesc tupleDesc = new TupleDesc(typeArr, nameArr);
+                HeapFile heapFile = new HeapFile(
+                        new File(baseFolder + "/" + tableName + ".dat"), tupleDesc);
+                addTable(heapFile, tableName, primaryKeyName);
+                System.out.println("Added table: " + tableName + " with schema " + tupleDesc);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.exit(0);
+        } catch (IndexOutOfBoundsException e) {
+            System.out.println("Invalid catalog entry: " + line);
+            System.exit(0);
+        }
+    }
 }
