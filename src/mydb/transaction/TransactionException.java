ee93c2cdc4d3a9f4b7c3ebceec86f848c941fb5c
Lann <3530306071@qq.com>
2023-08-15 21:35:13 +0800

    实现事务异常

---
+++ src/mydb/transaction/TransactionException.java
@@ -0,0 +1,15 @@
+package mydb.transaction;
+
+import java.lang.Exception;
+import java.io.Serial;
+
+/**
+ *  ¬ŒÒ“Ï≥£
+ */
+public class TransactionException extends Exception {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    public TransactionException() {}
+}
