bdfb15a5d6f52e55aa312ca9a030dff28dc91a57
Lann <3530306071@qq.com>
2023-08-21 21:12:37 +0800

    事务补充日志相关内容

--- src/mydb/transaction/Transaction.java
+++ src/mydb/transaction/Transaction.java
@@ -1,5 +1,9 @@
 package mydb.transaction;
 
+import mydb.common.Database;
+
+import java.io.IOException;
+
 /**
  * 事务（Transaction）类
  * 事务的四大特性（ACID）：原子性、一致性、隔离性、持久性
@@ -23,26 +27,43 @@ public class Transaction {
      */
     public void start() {
         started = true;
-        // TODO 异常捕获
+        try {
+            // 事务开始记录到日志文件
+            Database.getLogFile().logTransactionBegin(tid);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     /**
-     * 处理事务结束后（commit/rollback）的细节
+     * 事务结束后（commit/rollback）进行相关处理
+     * @param abort true则为事务发生异常终止，false则该事务正常结束提交（commit）
      */
-    public void over(boolean abort) {
+    public void end(boolean abort) throws IOException {
         if (started) {
             if (abort) {
-                // TODO ROLLBACK
-            } else {
-                // TODO COMMIT
+                Database.getLogFile().logAbort(tid);
             }
+            // 正常完成事务，释放事务锁
+            Database.getBufferPool().transactionComplete(tid, !abort);
+            if (!abort) {
+                Database.getLogFile().logCommit(tid);
+            }
+            started = false; // 事务结束
         }
-        // 事务执行结束
-        started = false;
     }
 
-//    public void commit();
-//
-//    public rollback();
+    /**
+     * 事务顺利完成，提交事务
+     */
+    public void commit() throws IOException {
+        end(false);
+    }
 
+    /**
+     * 事务异常（abort），进行事务回滚
+     */
+    public void rollback() throws IOException  {
+        end(true);
+    }
 }
