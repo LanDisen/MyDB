fafd729574fe09d2f5ba78aa82f6cd2d7cdf5c5c
Lann <3530306071@qq.com>
2023-08-21 20:57:37 +0800

    数据库添加获取日志文件的方法

--- src/mydb/common/Database.java
+++ src/mydb/common/Database.java
@@ -1,6 +1,9 @@
 package mydb.common;
 
 import mydb.storage.BufferPool;
+import mydb.storage.LogFile;
+
+import java.io.*;
 import java.util.concurrent.atomic.AtomicReference;
 
 /**
@@ -16,17 +19,40 @@ public class Database {
 
     private final BufferPool bufferPool;
 
+    private final static String LOG_FILE_NAME = "log";
+    private final LogFile logFile;
+
     private Database() {
         catalog = new Catalog();
         bufferPool = new BufferPool(BufferPool.DEFAULT_PAGES_NUM);
+        LogFile file = null;
+        try {
+            file = new LogFile(new File(LOG_FILE_NAME));
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
+        logFile = file;
     }
 
+    /**
+     * @return 获取数据库目录用来检索表
+     */
     public static Catalog getCatalog() {
         return instance.get().catalog;
     }
 
+    /**
+     * @return 获得该数据库系统的缓冲池对象
+     */
     public static BufferPool getBufferPool() {
         return instance.get().bufferPool;
     }
 
+    /**
+     * @return 返回该数据库系统的日志文件
+     */
+    public static LogFile getLogFile() {
+        return instance.get().logFile;
+    }
 }
